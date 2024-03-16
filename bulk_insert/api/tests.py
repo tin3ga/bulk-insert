2041f904d886223b7b59df2cf96875c3f472d4ed
tin3ga <118301331+tin3ga@users.noreply.github.com>
2024-03-16 21:09:08 +0300

    add Product Test Cases

--- bulk_insert/api/tests.py
+++ bulk_insert/api/tests.py
@@ -1,3 +1,60 @@
-from django.test import TestCase
+from .models import Product
+from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
+from rest_framework import status
+
 
 # Create your tests here.
+
+class ProductTestCase(APITestCase):
+    """
+    Test suite for Product
+    """
+
+    def setUp(self):
+        self.client = APIClient()
+        self.data = {
+            "name": "Product 1",
+            "image": "",
+            "variants": [
+                {
+                    "sku": "SKU001",
+                    "name": "Variant 1",
+                    "price": 19.99,
+                    "details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    "product_id": "1"
+                },
+            ]
+        }
+        self.url = "/products/"
+
+    def test_create_product(self):
+        """
+        test ProductViewSet create method
+        """
+        data = self.data
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Product.objects.count(), 1)
+        self.assertEqual(Product.objects.get().name, "Product 1")
+
+    def test_create_product_without_name(self):
+        """
+        test ProductViewSet create method when name is not in data
+        """
+        data = self.data
+        data.pop("name")
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_product_without_variant(self):
+        """
+        test ProductViewSet create method when name is not in data
+        """
+        data = self.data
+        data.pop("variants")
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+

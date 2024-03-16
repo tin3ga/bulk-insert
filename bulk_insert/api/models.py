5cda039ef8b453fafd08bc09e2e82ab2a4e3e98d
tin3ga <118301331+tin3ga@users.noreply.github.com>
2024-03-16 13:40:51 +0300

    Update models.py

--- bulk_insert/api/models.py
+++ bulk_insert/api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    # image = models.ImageField(upload_to="products/")
+    image = models.ImageField(upload_to="products/", null=True)
 
     def __str__(self):
         return self.name
@@ -16,4 +16,4 @@ class ProductVariant(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(decimal_places=2, max_digits=6)
     details = models.CharField(max_length=100)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, related_name='variants', on_delete=models.CASCADE)

41154e937dab5da77821f06c3666582c31a27d04
tin3ga <118301331+tin3ga@users.noreply.github.com>
2024-03-16 21:54:11 +0300

    Update admin.py

--- bulk_insert/api/admin.py
+++ bulk_insert/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from . models import Product, ProductVariant
+
 # Register your models here.
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(ProductVariant)
+class ProductVariant(admin.ModelAdmin):
+    list_display = ('id', 'sku', 'name', 'price', 'details', 'product_id')

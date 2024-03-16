f3fc8240be2e3857426c61cc575cf2982f20edea
tin3ga <118301331+tin3ga@users.noreply.github.com>
2024-03-16 13:41:35 +0300

    Update serializers.py

--- bulk_insert/api/serializers.py
+++ bulk_insert/api/serializers.py
@@ -2,13 +2,37 @@ from rest_framework import serializers
 from .models import ProductVariant, Product
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductVariant
-        fields = ["id", "name"]
+class ProductBulkCreateSerializer(serializers.ListSerializer):
+    def create(self, validated_data):
+        product_list = []
+        for item in validated_data:
+            product_list.append(Product(name=item['name'], image=item['image']))
+        products = Product.objects.bulk_create(product_list)
+        return products
+
+
+class VariantBulkCreateSerializer(serializers.ListSerializer):
+    def create(self, validated_data):
+        variant_list = []
+        for item in validated_data:
+            variant_list.append(
+                ProductVariant(sku=item['sku'], name=item['name'], price=item['price'], details=item['details'],
+                               product_id=item['product_id']))
+        product_variants = ProductVariant.objects.bulk_create(variant_list)
+        return product_variants
 
 
 class ProductVariantSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductVariant
-        fields = ["id", "sku", "name", "price", "details", "product_id"]
+        fields = '__all__'
+        list_serializer_class = VariantBulkCreateSerializer
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    variants = ProductVariantSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ["id", "name", "image", 'variants']
+        list_serializer_class = ProductBulkCreateSerializer
